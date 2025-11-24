111c9281e4d868af9ae45f30095eb398a4abdd98
lokeshec23 <lokesh.ec23@gmail.com>
2025-11-24 13:53:58 +0530

    feat: update LoginPage title to include 'AP'

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,9 @@
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Login Page AP</h1>
+    </div>
+  );
 };
 
 export default LoginPage;
