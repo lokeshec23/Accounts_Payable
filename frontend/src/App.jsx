b450b34a3129f9c8fdc2e7a51b559416156a6695
lokeshec23 <lokesh.ec23@gmail.com>
2025-11-24 13:45:26 +0530

    feat: implement initial login and registration pages with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { DatePicker } from 'antd';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
