4e6ad62d47d6931101bf77fcf7d92f34addfc3e2
Varshini110203 <vsakthivel@loandna.com>
2025-11-24 18:29:33 +0530

    feat: initialize FastAPI backend with authentication, CORS, and MongoDB connection setup.

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Accounts Payable API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_origins=["*"],  # React app URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
